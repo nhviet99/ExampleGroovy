ecb60e2eb25f21cf9f6a177e3791a04371b28603
Nguyen Hoang Viet <viethn.it@gmail.com>
2020-03-09 15:45:59 +0700

    second commit

---
+++ exam5.groovy
@@ -0,0 +1,11 @@
+import groovy.json.JsonSlurper 
+
+class Example {
+   static void main(String[] args) {
+      def jsonSlurper = new JsonSlurper()
+      def object = jsonSlurper.parseText('{ "name": "John", "ID" : "1"}') 
+		
+      println(object.name);
+      println(object.ID);
+   } 
+}
